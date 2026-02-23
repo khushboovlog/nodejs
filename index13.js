5d2331997c069ebb0d306e6f21b7571d61b29663
khushboovlog <shivshankargupta1972@gmail.com>
2026-02-23 13:42:20 +0530

    updated

---
+++ index13.js
@@ -0,0 +1,28 @@
+
+import express from 'express';
+import bcrypt from 'bcrypt';
+
+const app = express();
+app.listen(8080,()=>
+{
+    console.log("server is starting");
+});
+app.use(express.json());
+const users=[];
+app.post("/signup",(req,res)=>
+{
+    const body=req.body;
+    const hasPassword=bcrypt.hashSync(body.password,10);
+    body.password=hasPassword;
+    users.push(body);
+    res.json(users);
+}
+);
+app.post("/login",async (req,res)=>
+{
+    const {email,password}=req.body;    
+    const user=users.find((u)=>u.email===email);
+    const ckPassword=await bcrypt.compare(password,user.password);
+    if(ckPassword) res.send("login successful");
+    else res.send("invalid credentials");
+});
---
+++ index14.js
@@ -0,0 +1,17 @@
+
+import express from 'express';
+import jwt from 'jsonwebtoken';
+const secretKey = "mysecretkey";
+const app = express();
+app.listen(8080,()=>
+{
+    console.log("server is starting");
+});
+const user={
+    name:"john",
+    email:"john@example.com",
+    role:"student"
+}
+const token=jwt.sign(user,secretKey,{expiresIn:"1h"});
+console.log("Generated JWT:", token);
+//app.use(express.json());
